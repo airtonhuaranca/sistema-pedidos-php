2fe7ced0538f87763e86f58fcc6091ac3edc8ed7
Airton HE <airtondenilsonhuarancaechevarr@gmail.com>
2026-04-26 08:46:06 -0500

    Correccion conexion con tiDB en Vercel

--- api/conexion.php
+++ api/conexion.php
@@ -1,10 +1,16 @@
 <?php
-// CONFIGURACIÓN LOCAL PARA XAMPP
-$host = "127.0.0.1";
-$puerto = "3307";
-$bd = "sistema_pedidos";
-$usuario = "root";
-$password = "123123"; // tu contraseña de MySQL
+// CONEXIÓN PARA VERCEL + TiDB CLOUD
+
+$host = getenv("DB_HOST");
+$puerto = getenv("DB_PORT");
+$bd = getenv("DB_NAME");
+$usuario = getenv("DB_USER");
+$password = getenv("DB_PASSWORD");
+
+// Validar si faltan variables
+if (!$host || !$puerto || !$bd || !$usuario || !$password) {
+    die("Faltan variables de entorno en Vercel. Revisa DB_HOST, DB_PORT, DB_NAME, DB_USER y DB_PASSWORD.");
+}
 
 try {
     $conexion = new PDO(
@@ -14,8 +20,9 @@ try {
     );
 
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 } catch (PDOException $e) {
-    die("Error de conexión: " . $e->getMessage());
+    die("Error de conexión con host: " . $host . " puerto: " . $puerto . " base: " . $bd . " - " . $e->getMessage());
 }
 ?>
